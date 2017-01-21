aa96d03bfe762d20f0722654fe86f84c788c6d86
LanceCoffman <lanceacoffman@gmail.com>
2017-01-21 10:58:41 -0500

    checkin first hour training

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -7,19 +7,43 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Routing;
 
 namespace OdeToFood
 {
     public class Startup
     {
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -27,11 +51,48 @@ namespace OdeToFood
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler(new ExceptionHandlerOptions
+                {
+                    ExceptionHandler = context => context.Response.WriteAsync("Oops!")
+                });
+            }
+
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
+            app.UseFileServer(); //does default files and static files
+
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(ConfigureRoutes);
+
+            app.Run(ctx => ctx.Response.WriteAsync("Not found"));
+
+
+
+        //app.UseWelcomePage(new WelcomePageOptions
+        //{
+        //    Path = "/welcome"
+        //});
+
+        //app.Run(async (context) =>
+        //{
+        //    var message = greeter.GetGreeting();
+        //    await context.Response.WriteAsync(message);
+        //});
+    }
+
+        private void ConfigureRoutes(IRouteBuilder routeBuilder)
+        {
+            // how to pick apart the route of a url
+            //  /Home/Index
+            // Controller is automatically appended to Home
+
+
+            //curly brackets mean a parameter
+            //? means parameter is optional
+            routeBuilder.MapRoute("Default",
+                "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
